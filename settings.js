e876501b2e7321ca3321833018bd41675c3f8ed6
Soniartha Simarmata <soniarthasimarmata@gmail.com>
2021-07-17 16:05:03 +0700

    perubahan pada mapCenter dan iconHeight

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRJIzD7SEwLQGPwzsLgAoUf3wsV3on6VwlwPp3X-iWZPVm5YedTP8LnfYnqs2UA7a-FXWWIvZcYZbJP/pub?gid=0&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [0.6773991 , 101];
+const mapCenter = [0.6773991 , 100];
 const mapZoom = 14;
 
 // Marker icon height and width
-const iconHeight = 10;
-const iconWidth = 10;
+const iconHeight = 30;
+const iconWidth = 30;

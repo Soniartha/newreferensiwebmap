6354ae53b0baa38d0fba372d1ed3120bb0b7cf2e
Soniartha <65614408+Soniartha@users.noreply.github.com>
2021-07-17 12:34:25 +0700

    mengganti link csv dan cordinate initial map

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 // Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
 // or a Google Sheets URL (File > Publish to the Web > CSV )
-const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQCz1kNG2vxDke45Q0H8moWaFZZ-dhRSp34EKD_eMVWE-YjJTFDAcxdXydWSJ26JF4Ap4zlaZD1p9oi/pub?gid=0&single=true&output=csv';
+const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRJIzD7SEwLQGPwzsLgAoUf3wsV3on6VwlwPp3X-iWZPVm5YedTP8LnfYnqs2UA7a-FXWWIvZcYZbJP/pub?gid=0&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [41.65, -72.7];
-const mapZoom = 9;
+const mapCenter = [0.6 , 101];
+const mapZoom = 12;
 
 // Marker icon height and width
 const iconHeight = 40;

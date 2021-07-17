fb86565260e9057bea19a0819af145bf49ac517f
Soniartha Simarmata <soniarthasimarmata@gmail.com>
2021-07-17 14:14:31 +0700

    perubahan fly

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ var addMarkers = function(data) {
         placeInfo: d
       },
     ).on('click', function(e) {
-      map.flyTo(this._latlng, 15);
+      map.flyTo(this._latlng, 3);
       updateSidebar(this);
     });
 
